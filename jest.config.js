1e03d02ed053ee4cdee849e4453900d163dbc3bf
Technote <technote.space@gmail.com>
2021-02-17 16:04:55 +0900

    feat: update repository settings

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  clearMocks: true,
+  moduleFileExtensions: ['js', 'ts'],
+  testMatch: ['**/*.test.ts'],
+  testRunner: 'jest-circus/runner',
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  verbose: true,
+  coverageDirectory: '<rootDir>/coverage',
+};
